67a2d3fd9b45745d542df368dd414c83eb31fcad
chosoz <67200206+chosoz@users.noreply.github.com>
2022-10-19 19:18:41 +0800

    Update and rename 删除历史版本 to 删除历史版本.cs

--- `  Others/删除历史版本.cs
+++ `  Others/删除历史版本.cs
@@ -15,4 +15,4 @@ namespace OneDriveVersionCleaner
            // IMPORTANT: This is not production code, and it deletes files from your cloud storage. Use at your own risk!
            // ------------------------------------------------------------------------------------------------------------
 
-           // TODO: Configure these values.
+           // TODO: Configure these values. 
