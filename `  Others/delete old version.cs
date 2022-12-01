d4667ab4d21c919fdbed0cb0c744fc5e2993a47d
chosoz <67200206+chosoz@users.noreply.github.com>
2022-12-01 21:03:20 +0800

    Update delete old version.cs

--- `  Others/delete old version.cs
+++ `  Others/delete old version.cs
@@ -29,4 +29,17 @@ namespace OneDriveVersionCleaner
            string username = "
            SecureString password = new NetworkCredential("", ",              
            // This is not a TODO, 
+                                                         
+           /*
+           Embedded AI￤嵌入式人工智能
+
+关键技术： 视频监控 物体识别
+
+适合人群： 大学生 在职想转型升级的
+
+类似项目： 银行监控 罪犯追踪
+
+视觉AI 方向 物体识别 数据训练 调参 优化 opencv 图像显示 opengl 图像处理 
+*/
+                                                         
                                                           
