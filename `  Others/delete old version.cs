935de5bcb56f07c482cc26bb85a8fd8ba3af772b
chosoz <67200206+chosoz@users.noreply.github.com>
2023-01-02 23:45:02 +0800

    Update delete old version.cs

--- `  Others/delete old version.cs
+++ `  Others/delete old version.cs
@@ -47,4 +47,4 @@ namespace OneDriveVersionCleaner
            string webFullUrl = "
            string username = "
            SecureString password = new NetworkCredential            
-           // This is not a test, not funtional                                                         
+           // This is only a test, not funtional                                                         
